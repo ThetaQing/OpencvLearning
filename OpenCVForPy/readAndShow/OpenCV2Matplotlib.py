6d63082f572512fdddc53b1c944e7c288b639706
ThetaQing <csusq2016@163.com>
2020-01-07 22:05:03 +0800

    读取和显示基本操作，包括OpenCV和matplotlib的转换

--- OpenCVForPy/readAndShow/OpenCV2Matplotlib.py
+++ OpenCVForPy/readAndShow/OpenCV2Matplotlib.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan  7 17:08:07 2020
-
+Introduction: 比较并转换OpenCV和Matplotlib对彩色图像的处理，前者的排序是BGR，后者是RGB
 @author: User
 """
 
@@ -32,5 +32,20 @@ plt.show()
 docPltImshow = help(plt.imshow)
 print(docPltImshow)  # 打印plt模块下的imshow函数的帮助文档
 '''
+'''
+# 解决办法
+1、先split再merge
+2、直接翻转img[:,:,::-1]
+'''
+b, g, r = cv2.split(img)
+img2 = cv2.merge([r, g, b])
+img3 = img[:,:,::-1] # 直接翻转，更快
+plt.subplot(131); plt.imshow(img) # 显示错误的输出图像
+plt.subplot(132); plt.imshow(img2) # 显示正确的输出图像
+plt.subplot(133); plt.imshow(img3) # 显示正确的输出图像
+plt.show()
 
-
+cv2.imshow('BGR Image:', img)  # 正确的输出图像
+cv2.imshow('RGB Image:', img2) # 错误的输出图像
+cv2.waitKey(0)
+cv2.destroyAllWindows()
